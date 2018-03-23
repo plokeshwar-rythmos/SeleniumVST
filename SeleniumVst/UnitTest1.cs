88dcce3175e3e7d9b9019f41b73be206ec0565bd
plokeshwar-rythmos <plokeshwar@rythmos.com>
2018-03-23 15:08:41 +0530

    Initial Commit

---
+++ SeleniumVst/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumVst
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+        [TestMethod]
+        [TestCategory("IE")]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            System.Threading.Thread.Sleep(5000);
+
+            driver.FindElement(By.Id("sb_form_q")).SendKeys("VSTS");
+            System.Threading.Thread.Sleep(5000);
+
+            driver.FindElement(By.Id("sb_form_go")).Click();
+            System.Threading.Thread.Sleep(5000);
+
+            //driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+            System.Threading.Thread.Sleep(5000);
+            Assert.IsTrue(driver.Title.Contains("VSTS"), "Verified title of the page");
+        }
+
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            appURL = "http://www.bing.com/";
+
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
